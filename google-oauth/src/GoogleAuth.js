47efe81132f56ee7007ed0869c288d9d2942354b
Arghun-dev <arghun.developer@gmail.com>
2020-03-27 12:30:56 +0430

    Setting up GoogleAuth

--- google-oauth/src/GoogleAuth.js
+++ google-oauth/src/GoogleAuth.js
@@ -1,6 +1,14 @@
 import React from 'react';
 
 class GoogleAuth extends React.Component {
+    componentDidMount() {
+        window.gapi.load('client:auth2', () => {
+            window.gapi.client.init({
+                clientId: '725154706985-f9hp80v6uh7a6p2o86cghbmmp88k4une.apps.googleusercontent.com',
+                scope: 'email'
+            })
+        })
+    }
     render() {
         return (
             <div>GoogleAuth</div>
